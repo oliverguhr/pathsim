aed4dfadd74adac222cc2e2a11378d3179cc15d6
Oliver Guhr <oliver.guhr@gmx.de>
2016-03-26 15:20:20 +0100

    added simple angular based

---
+++ scripts/app.js
@@ -0,0 +1,26 @@
+/*jshint esversion: 6 */
+var app = angular.module('pathsim', []);
+
+
+/* Change map from regular js
+angular.element($0).scope().$apply((scope) => {scope.map.map[0][0].isBlocked = scope.map.map[0][0].isBlocked!})
+*/
+
+
+app.controller('MapController', function($attrs) {
+  var map = this;
+  map.name = "test";
+
+  var data = new Map($attrs.rows, $attrs.cols);
+
+  map.cellSize = 25;
+  map.widthPx = data.cols * map.cellSize;
+  map.heightPx = data.rows * map.cellSize;
+
+  map.map = data.grid;
+
+
+  map.addRandomObstacles = () => {
+    data.addRandomObstacles((data.cols*data.rows)*0.1);
+  };
+});
---
+++ scripts/map.js
@@ -0,0 +1,68 @@
+/*jshint esversion: 6 */
+
+class Map {
+  constructor(rows, cols) {
+    this.rows = rows;
+    this.cols = cols;
+
+    this.grid = [
+      []
+    ];
+
+    this.initializeGrid();
+  }
+
+  initializeGrid() {
+    for (var row = 0; row < this.rows; row++) {
+      this.grid.push([]);
+      for (var col = 0; col < this.cols; col++) {
+        this.grid[row].push(new Cell(row, col));
+      }
+    }
+  }
+
+  addRandomObstacles(count) {
+
+    for (var i = 0; i < count; i++) {
+      var row = Random.getRandomInt(0, this.rows - 1);
+      var col = Random.getRandomInt(0, this.cols - 1);
+      this.grid[row][col].isBlocked = true;
+    }
+  }
+}
+
+class Cell {
+  constructor(row, col, isBlocked = false) {
+    this.row = row;
+    this.col = col;
+
+    this.isBlocked = isBlocked;
+    this.isClosed = false;
+    this.isCurrent = false;
+  }
+}
+
+class Random {
+  /**
+   * Get a random floating point number between `min` and `max`.
+   *
+   * @param {number} min - min number
+   * @param {number} max - max number
+   * @return {float} a random floating point number
+   */
+  static getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  /**
+   * Get a random integer between `min` and `max`.
+   *
+   * @param {number} min - min number
+   * @param {number} max - max number
+   * @return {int} a random integer
+   */
+  static getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+}
