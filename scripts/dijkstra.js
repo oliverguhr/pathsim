5171458669fa0ecb4f771530ba855d831fcf8a28
Oliver Guhr <oliver.guhr@gmx.de>
2016-04-23 18:50:17 +0200

    removed redudant abs call

--- scripts/dijkstra.js
+++ scripts/dijkstra.js
@@ -4,15 +4,15 @@ class Distance {
     }
 
     static euklid(previousCell, currentCell) {
-      let x = Math.abs(previousCell.position.x - currentCell.position.x);
-      let y = Math.abs(previousCell.position.y - currentCell.position.y);
+      let x = previousCell.position.x - currentCell.position.x;
+      let y = previousCell.position.y - currentCell.position.y;
       return Math.sqrt( x*x + y*y );
     }
 
     static diagonalShortcut(previousCell, currentCell) {
       /* See  http://www.policyalmanac.org/games/heuristics.htm  for details*/
-      let xDistance = Math.abs(previousCell.position.x - currentCell.position.x)
-      let yDistance = Math.abs(previousCell.position.y - currentCell.position.y)
+      let xDistance = Math.abs(previousCell.position.x - currentCell.position.x);
+      let yDistance = Math.abs(previousCell.position.y - currentCell.position.y);
       if (xDistance > yDistance)
       {
         return  14*yDistance + 10*(xDistance-yDistance);
